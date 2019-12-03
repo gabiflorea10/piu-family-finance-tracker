86d6fbbca8f22559c48e3c9c0a714bbf5bee7f12
gabiflorea10 <floreagabi_fga@yahoo.com>
2019-12-03 23:17:30 +0200

    Create login page

--- js/1_login.js
+++ js/1_login.js
@@ -1,2 +1,18 @@
 $(document).ready(() => {
+
+    $('#login-button').on('click', function () {
+        let userName = $('#login-username').val();
+        let password = $('#login-password').val();
+
+        console.log(userName);
+        console.log(password);
+
+        if (userName === 'parent' && password === "parent") {
+            window.location = '/piu-family-finance-tracker/html/alta_pagina.html';
+        } else {
+            alert('User sau parola gresite');
+        }
+    });
+
+
 });
