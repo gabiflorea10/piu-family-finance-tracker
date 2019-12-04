327a2af79d65ae9815af7d059028d76c306cdca0
gabiflorea10 <floreagabi_fga@yahoo.com>
2019-12-04 22:14:36 +0200

    Create main pages

--- js/1_login.js
+++ js/1_login.js
@@ -7,9 +7,13 @@ $(document).ready(() => {
         console.log(userName);
         console.log(password);
 
-        if (userName === 'parent' && password === "parent") {
-            window.location = '/piu-family-finance-tracker/html/alta_pagina.html';
-        } else {
+        if (userName === "parent" && password === "parent") {
+            window.location = '/piu-family-finance-tracker/html/2_1_pagina_principala_adult.html';
+        }
+        else if(userName === "copil" && password === "copil"){
+            window.location = '/piu-family-finance-tracker/html/2_2_pagina_principala_copil.html';
+        }
+        else {
             alert('User sau parola gresite');
         }
     });
